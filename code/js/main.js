2dadc6c0669b1da1fb0e5f6ea02e66bb9322d009
fanlepian <72285411+fanlepian1@users.noreply.github.com>
2022-09-03 18:24:08 +0800

    Add files via upload

---
+++ code/js/main.js
@@ -0,0 +1,8 @@
+var codedata = [
+    {
+        title: "第一个",
+        link: "#",
+        video: "#"
+    }
+];
+var muban = '<h2 data-flags="title">'+codedata[0].title+'</h2><p class="p"></p><a href="'+codedata[0].link+'" class="p" data-flags="link">点我下载代码</a><br /><a href="'+codedata[0].video+'" class="p"  data-flags="video">观看改代码的演示视频</a>'
